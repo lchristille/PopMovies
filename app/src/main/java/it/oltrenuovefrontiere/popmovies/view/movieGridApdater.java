ebf706a12fd4e3d4ccd6d1d5e169178273a32b0a
Luca Christille <lucachristille@me.com>
2017-02-15 16:52:46 +0100

    ...

---
+++ app/src/main/java/it/oltrenuovefrontiere/popmovies/view/movieGridApdater.java
@@ -0,0 +1,57 @@
+package it.oltrenuovefrontiere.popmovies.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import it.oltrenuovefrontiere.popmovies.R;
+import it.oltrenuovefrontiere.popmovies.model.Movie;
+
+/**
+ * Created by NewArt on 15/02/2017.
+ */
+
+// https://www.raywenderlich.com/126528/android-recyclerview-tutorial
+
+public class movieGridApdater extends RecyclerView.Adapter<movieGridApdater.ViewHolder> {
+
+    private List<Movie> mMovieList;
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMovieList.size();
+    }
+
+    public movieGridApdater(List<Movie> list) {
+        super();
+        mMovieList = list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View inflatedView = LayoutInflater.from(parent.getContext()).inflate(R.layout.filmcard, parent, false);
+        return new ViewHolder(inflatedView);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mTvTitle;
+        TextView mTvPoster;
+        public ViewHolder(View v) {
+            super(v);
+            this.mTvTitle = (TextView) v.findViewById(R.id.tv_title);
+            this.mTvPoster = (TextView) v.findViewById(R.id.tv_poster);
+        }
+
+    }
+
+}
